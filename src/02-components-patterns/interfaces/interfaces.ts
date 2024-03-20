b7638f3dde4795863b26205c5ef5f0702728aaf0
Sebastián Cuello <sebastiangcuello@hotmail.com>
2024-03-20 18:28:39 -0300

    Coponent Extend Styles - listo

--- src/02-components-patterns/interfaces/interfaces.ts
+++ src/02-components-patterns/interfaces/interfaces.ts
@@ -1,10 +1,7 @@
-import { ReactElement } from "react";
-
-
-export interface ProductCardProps {
-    product: Product;
-    children?: ReactElement | ReactElement[];
-}
+import { Props as ProductButtonsProps } from "../components/ProductButtons";
+import { Props as ProductCardProps } from "../components/ProductCard";
+import { Props as ProductImageProps } from "../components/ProductImage";
+import { Props as ProductTitleProps } from "../components/ProductTitle";
 
 export interface Product {
     id: string;
@@ -14,13 +11,13 @@ export interface Product {
 
 export interface ProductContextProps {
     counter: number;
-    increaseBy: (value: number) => void;
     product: Product;
+    increaseBy: (value: number) => void; 
 }
 
 export interface ProductCardHOCProps {
-    ({ children, product }: ProductCardProps) : JSX.Element,
-    Image: ({ img }: { img?: string }) => JSX.Element;
-    Title: ({ title }: { title?: string }) => JSX.Element;
-    Buttons: () => JSX.Element;
+    ({ children, product, className }: ProductCardProps ) : JSX.Element,
+    Image:   ( Props : ProductImageProps ) => JSX.Element;
+    Title:   ( Props : ProductTitleProps ) => JSX.Element;
+    Buttons: ( Props : ProductButtonsProps) => JSX.Element;
 }
--- src/02-components-patterns/pages/ShoppingPage.tsx
+++ src/02-components-patterns/pages/ShoppingPage.tsx
@@ -1,5 +1,5 @@
 import { ProductButtons, ProductCard, ProductImage, ProductTitle } from "../components"
-import noImage from '../assets/no-image.jpg'
+import '../styles/custom-styles.css';
 
 const product = {
     id: '1',
@@ -9,7 +9,7 @@ const product = {
 
 export const ShoppingPage = () => {
   return (
-    <div>
+    <div >
         <h1>Shopping Store</h1>
         <hr />
         <div style={{
@@ -18,16 +18,36 @@ export const ShoppingPage = () => {
             flexWrap: 'wrap',
         }}>
         
-        <ProductCard product= { product } >
-            <ProductCard.Image />
-            <ProductCard.Title title= { 'Hola'} />
-            <ProductCard.Buttons />
+        <ProductCard 
+            product= { product }  
+            className="bg-dark text-white"
+        >
+            <ProductCard.Image className="custom-image"  />
+            <ProductCard.Title title= { 'Coffe Mug 2'} className="text-bold" />
+            <ProductCard.Buttons className="custom-buttons"/>
         </ProductCard> 
 
-        <ProductCard product= { product } >
-            <ProductImage img= { noImage } />
-            <ProductTitle />
-            <ProductButtons />
+        <ProductCard 
+            product= { product } 
+            className="bg-dark text-white"
+        >
+            <ProductImage className="custom-image" style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }} />
+            <ProductTitle className="text-bold" />
+            <ProductButtons className="custom-buttons" />
+        </ProductCard> 
+
+        <ProductCard 
+            product= { product } 
+            style={{
+                backgroundColor: "#70D1F8"
+            }}
+        >
+            <ProductImage style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }} />
+            <ProductTitle style={{ fontWeight: 'bold' }} />
+            <ProductButtons style= {{
+                display: 'flex',
+                justifyContent: 'end'
+            }}/>
         </ProductCard> 
 
         </div>
